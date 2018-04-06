7b47fe5d931e5329608782085f86e7081f0b55d2
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-04-06 10:14:37 +0530

    Create 01.cpp

---
+++ set2/01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   string str;
+   cin>>str;
+   if((str[0]=='s')||(str[0]=='S'))
+   {
+       cout<<"yes";
+   }else{
+       cout<<"no";
+   }
+}
