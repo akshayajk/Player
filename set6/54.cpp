d1084bc8acf526a792400f071c56ae9f21b81442
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-05-23 15:53:10 +0530

    Create 54.cpp

---
+++ set6/54.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str,str2;
+    cin>>str>>str2;
+    
+    if(str==str2)
+    {
+        cout<<"yes";
+    }
+        else
+        {
+        cout<<"no";
+    }
+}
